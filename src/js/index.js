305868e18b557e0e4ca3d074864e46e53255129f
BurritoBurrito <110749874+BurritoBurrito@users.noreply.github.com>
2025-11-25 20:41:56 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -25,24 +25,3 @@ function darkmode() {
     document.body.classList.toggle('dark-mode', localStorage.getItem('darkmode') === 'true');
     
   }
-
- // Create a script element
-const script = document.createElement('script');
-
-// Set the data-grow-initializer attribute
-script.setAttribute('data-grow-initializer', '');
-
-// Add the script content inside a self-executing function
-script.textContent = `!(function(){
-  window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));
-  var e=document.createElement("script");
-  e.type="text/javascript";
-  e.src="https://faves.grow.me/main.js";
-  e.defer=true;
-  e.setAttribute("data-grow-faves-site-id","U2l0ZTpmYzVhOGUxYS03NTJlLTRiNTQtOGFjMy1mYWNmYWJhMjMxMmM=");
-  var t=document.getElementsByTagName("script")[0];
-  t.parentNode.insertBefore(e,t);
-})();`;
-
-// Append the script to the end of the body
-document.body.appendChild(script);
