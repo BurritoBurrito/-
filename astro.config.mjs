1d9c2ec05c1b41878c26aa5a28e36d72a6eedb27
Blue Calculator <85308130+BlueCalculator@users.noreply.github.com>
2026-02-16 21:00:05 -0500

    Added cloudflare module, fixed typo.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,12 @@ import Compress from 'astro-compress';
 
 import compress from 'vite-plugin-compression';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
   site: 'https://burritoedition.com',
   integrations: [
     vue(),
